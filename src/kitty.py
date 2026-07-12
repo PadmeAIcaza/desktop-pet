846a62c664a9b4ba38ab9a2fa035d13c11339bd5
spicypusheen <padme.garrapata@gmail.com>
2026-07-12 15:32:27 -0500

    Code refactor (to hold more sprite sheets)

--- src/kitty.py
+++ src/kitty.py
@@ -9,28 +9,41 @@ class Kitty:
         # sprite sheet info (384x64, so 6 frames of 64x64
         self.frame_width = 64
         self.frame_height = 64
-        self.frame_count = 6
+        # pet's dimensions
+        self.width = self.frame_width
+        self.height = self.frame_height
         self.current_frame = 0
+        # stores all of the pet's animations
+        self.animations = {}
+        self.animations['idle'] = self.load_animation(sprite_path)
+        self.current_animation = 'idle'
 
-        # open sprite sheet using Pillow
-        sprite_sheet = Image.open(sprite_path)
-        # holds every separate frame
-        self.frames = []
-
-        for index in range(self.frame_count):
-            left = index*self.frame_width # frame 0 starts at x=0, frame 1 starts at x=64, frame 3 starts at x=128
-            top = 0
-            right = left+self.frame_width
-            bottom = self.frame_height
-
-            frame = sprite_sheet.crop((left, top, right, bottom))
-            self.frames.append(ImageTk.PhotoImage(frame))
-
-        self.label = tk.Label(self.window, image=self.frames[0], bg='magenta', borderwidth=0)
-
+        self.label = tk.Label(self.window, image=self.animations["idle"][0], bg="magenta", borderwidth=0)
         self.label.pack()
 
+    def load_animation(self, sprite_path):
+        # open sprite sheet using Pillow
+        sprite_sheet = Image.open(sprite_path)
+        frame_count = sprite_sheet.width // self.frame_width
+        # holds every separate frame
+        frames = []
+        for index in range(frame_count):
+            left = index * self.frame_width # frame 0 starts at x=0, frame 1 starts at x=64, frame 3 starts at x=128
+            top = 0 # the sheet is only one row tall, so every frame starts at the top
+            right = left + self.frame_width
+            bottom = self.frame_height
+            # crop ONE frame out of the sprite sheet
+            frame = sprite_sheet.crop((left, top, right, bottom))
+            # then convert it into a Tkinter image and store it
+            frames.append(ImageTk.PhotoImage(frame))
+        return frames
+
     def animate(self):
-        self.current_frame = (self.current_frame + 1) % len(self.frames)
-        self.label.configure(image=self.frames[self.current_frame])
+        # get the frames for the animation currenlty playing
+        frames = self.animations[self.current_animation]
+        # move to the next frame, when the last frame is reached, return to frame 0
+        self.current_frame = (self.current_frame + 1) % len(frames)
+        # update the image displayed by the label
+        self.label.configure(image=frames[self.current_frame])
+
         self.window.after(150, self.animate)
--- src/main.py
+++ src/main.py
@@ -4,9 +4,10 @@ from kitty import Kitty
 
 # main window
 window = tk.Tk()
+screen_width = window.winfo_screenwidth()  # 1536
+screen_height = window.winfo_screenheight() # 864
 
 window.title("Desktop Pet")
-
 # # window width and height
 # window.geometry("300x300")
 # removes window decorations
@@ -19,9 +20,15 @@ transparent_color = 'magenta'
 window.configure(bg=transparent_color)
 window.attributes('-transparentcolor', transparent_color)
 
+# create the pet object
 pet = Kitty(window, '../assets/sprites/Idle.png')
+#
+x = (screen_width - pet.height) // 2
+y = (screen_height  - 48) - pet.height
+window.geometry(f"+{x}+{y}")
 pet.animate()
 
+# closes the app after pressing esc
 window.bind("<Escape>", lambda event: window.destroy())
 
 window.mainloop()
